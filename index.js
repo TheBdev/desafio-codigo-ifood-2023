6604a9209aa73d6a4b809a2f4ef2019d3e3ebfac
TheBdev <andyteacher@hotmail.com>
2023-11-28 23:31:12 -0300

    comitei o programa index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+let nome = "Jaspion"
+let xp = 999
+if (xp <1000){
+    let nivel = "Ferro"
+    console.log("O herói de nome " + nome + " está no nível de " + nivel + ".")   
+}
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let nome = "jaspion"
+let xp = 1000
+let nivelF = 1000
+if (xp <=1000){
+    let nivel = "Ferro"
+    console.log("O herói de nome " + nome + " está no nível de " + nivel)   
+}
