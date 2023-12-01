427cfc8b1cc8c26004ebc48ff537a690f7f56841
TheBdev <andyteacher@hotmail.com>
2023-12-01 12:26:07 -0300

    Deletei arquivos não necessários.

--- index.js
+++
@@ -1,7 +0,0 @@
-let nome = "Jaspion"
-let xp = 999
-if (xp <1000){
-    let nivel = "Ferro"
-    console.log("O herói de nome " + nome + " está no nível de " + nivel + ".")   
-}
-
--- tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-let nome = "jaspion"
-let xp = 1000
-let nivelF = 1000
-if (xp <=1000){
-    let nivel = "Ferro"
-    console.log("O herói de nome " + nome + " está no nível de " + nivel)   
-}
